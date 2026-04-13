f31ba012c95178a9fad9f131e7b4d6ce0deb2f05
MattRHall <Matt.hall85@googlemail.com>
2026-04-13 13:24:38 +0100

    env removal

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,36 +40,12 @@ const config: any = {
     networks: {
         avalanche_fuji: {
             url: `${process.env.TEST_AVAX_RPC_URL}`,
-            accounts: [
-                `0x${process.env.TEST_AVAX_DEPLOYER_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_SUPERADMIN_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_GOVERNOR_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_MANAGER_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_AUTHORIZER_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_SIGNER1_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_SIGNER2_PRIVATE_KEY}`,
-                `0x${process.env.TEST_AVAX_SIGNER3_PRIVATE_KEY}`,
-            ],
         },
         avalanche: {
             url: `${process.env.PROD_AVAX_RPC_URL}`,
-            accounts: [
-                `0x${process.env.PROD_DEPLOYER_PRIVATE_KEY}`,
-                `0x${process.env.PROD_GOVERNOR_PRIVATE_KEY}`,
-                `0x${process.env.PROD_MANAGER_PRIVATE_KEY}`,
-            ],
         },
         tron_shasta: {
             url: `${process.env.TEST_TRON_RPC_URL}`,
-            accounts: [
-                `0x${process.env.TEST_TRON_DEPLOYER_PRIVATE_KEY}`,
-                `0x${process.env.TEST_TRON_SUPERADMIN_PRIVATE_KEY}`,
-                `0x${process.env.TEST_TRON_GOVERNOR_PRIVATE_KEY}`,
-                `0x${process.env.TEST_TRON_MANAGER_PRIVATE_KEY}`,
-                `0x${process.env.TEST_TRON_SIGNER1_PRIVATE_KEY}`,
-                `0x${process.env.TEST_TRON_SIGNER2_PRIVATE_KEY}`,
-                `0x${process.env.TEST_TRON_SIGNER3_PRIVATE_KEY}`,
-            ],
         },
         //sepolia_test: {
         //    url: `${process.env.SEPOLIA_TEST_URL}`,
@@ -81,16 +57,6 @@ const config: any = {
         //},
         localhost: {
             url: "http://localhost:8545",
-            //accounts: [
-            //  `0x${process.env.STAGING_DEPLOYER_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_ADMIN_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_GOVERNOR_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_DEVELOPER_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_LENDER_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_LOAN_MANAGER_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_SUPERADMIN_PRIVATE_KEY}`,
-            //  `0x${process.env.STAGING_BORROWER_PRIVATE_KEY}`,
-            //],
         },
     },
     etherscan: {
